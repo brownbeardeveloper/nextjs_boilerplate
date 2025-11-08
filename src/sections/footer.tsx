0b1b55129ff409265d0cd281289bf75a822d8bf2
Björn Bergström <bjorn.bergstr@outlook.com>
2025-11-08 13:40:11 +0100

    fix: footer

--- src/sections/footer.tsx
+++ src/sections/footer.tsx
@@ -7,13 +7,13 @@ import Link from "next/link";
 export default function Footer() {
 
     const linkUnderline = "absolute bottom-3 left-0 w-full h-0.5 transition-all duration-200 rounded \
-                                    bg-blue-500 scale-x-0 origin-left \
+                                    bg-white scale-x-0 origin-left \
                                     transition-transform duration-300 group-hover:scale-x-100";
 
     const linkClasses = "block w-full h-full py-5 px-1.5 transform transition-all duration-200 \
                                     tracking-wide uppercase \
                                     text-white \
-                                    group-hover:text-blue-400";
+                                    group-hover:text-white";
 
     const menuLinks = [
         { url: "/about", label: "Our Story" },
@@ -22,24 +22,29 @@ export default function Footer() {
     ];
 
     const socialLinks = [
-        { url: "/facebook", label: <FacebookIcon className="" size={24} /> },
-        { url: "/instagram", label: <InstagramIcon className="" size={24} /> },
-        { url: "/twitter", label: <TwitterIcon className="" size={24} /> },
+        { url: "/#", label: <FacebookIcon className="" size={24} /> },
+        { url: "/#", label: <InstagramIcon className="" size={24} /> },
+        { url: "/#", label: <TwitterIcon className="" size={24} /> },
     ];
 
     return (
         <footer>
-            <div className="h-30"/>
-            <div className="w-full min-h-screen px-5 flex items-center justify-center bg-gradient-to-b from-gray-200 to-gray-400">
-                <div className="md:w-2/3 w-full px-4 text-gray-700 flex flex-col">
+            <div className="w-full min-h-screen px-5 flex items-center justify-center bg-gradient-to-t from-gray-400 to-gray-300">
+                <div className="md:w-2/3 w-full px-4 text-white flex flex-col">
                     <div className="w-full text-7xl font-bold">
                         <p className="w-full">
-                        Discover our vision for personal growth.
+                        Lorem ipsum dolor sit amet.
                         </p>
                     </div>
                     <div className="flex mt-8 flex-col justify-center items-start gap-y-5 gap-x-10 md:flex-row md:justify-between md:items-center">
-                        <p className="w-full md:w-2/3 text-gray-700">
-                        We all lose our way sometimes, rushing through the days and forgetting why we started. Innerloop helps you slow down and reconnect with what truly matters. With gentle structure and simple weekly reflection, it becomes easier to build clarity and direction. This is your space to think clearly, act with intention and grow at your own pace. When life gets loud again you will know exactly where to return, to yourself.
+                        <p className="w-full md:w-2/3 text-white">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, 
+                            justo at blandit feugiat, libero augue convallis lorem, sit amet 
+                            consequat urna ipsum non nunc. Integer nec magna eu lorem tincidunt 
+                            suscipit et a erat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, 
+                            justo at blandit feugiat, libero augue convallis lorem, sit amet 
+                            consequat urna ipsum non nunc. Integer nec magna eu lorem tincidunt 
+                            suscipit et a erat.
                         </p>
                     </div>
                     <nav className="flex flex-col">
@@ -68,8 +73,8 @@ export default function Footer() {
                                 ))}
                             </div>
                         </ul>
-                        <hr className="border-gray-700" />
-                        <p className="w-full text-center my-8 text-gray-700 text-xs tracking-wide">
+                        <hr className="border-white" />
+                        <p className="w-full text-center my-8 text-white text-xs tracking-wide">
                             &copy; 2025 Frontend Template. All rights reserved. | {" "}
                             <Link href="/copyright" className="hover:text-blue-500 transition-colors underline">
                             Copyright & Terms
